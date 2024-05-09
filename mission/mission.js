f9f6bd6a54824b07483fc8d780a611d1f0bb5e4e
giovannalizz <cru23026@byui.edu>
2024-05-09 11:11:17 -0600

    adding

--- mission/mission.js
+++ mission/mission.js
@@ -1,18 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const themeSelector = document.getElementById('theme-selector');
-  const bodyElement = document.body; 
-  const logo = document.querySelector('.logo'); 
+document.addEventListener("DOMContentLoaded", function () {
+  const themeSelector = document.getElementById("theme-selector");
+  const bodyElement = document.body;
+  const logo = document.querySelector(".logo");
 
   function changeTheme() {
-      if (themeSelector.value === 'dark') {
-          bodyElement.classList.add('dark');
-          logo.src = "./byui-logo_dark.png"; 
-      } else {
-          bodyElement.classList.remove('dark');
-          logo.src = './byui-logo_blue.jpeg';
-      }
+    if (themeSelector.value === "dark") {
+      bodyElement.classList.add("dark");
+      logo.src = "./byui-logo_dark.png";
+    } else {
+      bodyElement.classList.remove("dark");
+      logo.src = "./byui-logo_blue.jpeg";
+    }
   }
 
-  themeSelector.addEventListener('change', changeTheme);
+  themeSelector.addEventListener("change", changeTheme);
 });
-
